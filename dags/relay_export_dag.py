f9910eafd33e17b105fd3be64b6b801117fc3f8b
Gui Heise <gheise@ufl.edu>
2022-10-17 13:24:18 -0400

    Reduce to one task

--- dags/relay_export_dag.py
+++ dags/relay_export_dag.py
@@ -8,11 +8,11 @@ POSTGRES_CONN_ID = "boost_relay_read_replica"
 with DAG(
     dag_id = DAG_ID,
     start_date = datetime.datetime(2022,10,17),
-    schedule_interval = "@hourly",
+    schedule_interval = "@once",
     catchup = True,
 ) as dag:
 
-    def _get_relay_data(ti):
+    def _fetch_and_export_relay_data():
 
         hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         connection = hook.get_conn()
@@ -20,37 +20,19 @@ with DAG(
             cursor.execute(f"""
                 SELECT * 
                 FROM mainnet_builder_block_submission 
-                WHERE inserted_at > {datetime.datetime(2022,10,17)}
+                WHERE inserted_at > '{datetime.datetime(2022,10,17)}'
+                LIMIT 1
             """)
             result = cursor.fetchall()
 
-        ti.xcom_push(key="block_query_result",value=result)
-    
-    def _export_to_s3(ti):
-        pass
-
-    def _export_to_rds(ti):
         
         hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         connection = hook.get_conn()
         with connection.cursor() as cursor:
             cursor.execute(f"")
 
-        ti.xcom_pull(key="block_query_result",task_ids="get_relay_data")
-
-
-    get_relay_data = PythonOperator(
-    task_id="get_relay_data",
-    python_callable=_get_relay_data
+    fetch_and_export_relay_data = PythonOperator(
+    task_id="fetch_and_export_relay_data",
+    python_callable=_fetch_and_export_relay_data
     )
-    export_to_s3 = PythonOperator(
-        task_id="export_to_s3",
-        python_callable=_export_to_s3
-    )
-    export_to_rds= PythonOperator(
-        task_id="export_to_rds",
-        python_callable=_export_to_rds
-    )
-
-
-    get_relay_data >> export_to_s3 >> export_to_rds
+    fetch_and_export_relay_data 
