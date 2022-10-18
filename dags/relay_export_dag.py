f2c78dd83f7641253002d69dfbe485a7fc8cb258
Gui Heise <gheise@ufl.edu>
2022-10-18 13:58:26 -0400

    Add bucket name secret

--- dags/relay_export_dag.py
+++ dags/relay_export_dag.py
@@ -1,10 +1,19 @@
 import datetime
+import boto3
+import json
 from airflow import DAG
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.python import PythonOperator
 DAG_ID = "relay_export_dag"
 POSTGRES_CONN_ID = "boost_relay_read_replica"
+AWS_REGION = "us-east-2"
+
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager',region_name=AWS_REGION)
+secret = json.loads(client.get_secret_value(SecretId=SECRET)["SecretString"])
+
+BUCKET_NAME = secret["bucket_name"]
 
 with DAG(
     dag_id = DAG_ID,
@@ -19,19 +28,19 @@ with DAG(
         connection = psql_hook.get_conn()
         with connection.cursor() as cursor:
             cursor.execute(f"""
-                SELECT * 
-                FROM mainnet_builder_block_submission 
+                SELECT *
+                FROM mainnet_builder_block_submission
                 WHERE inserted_at > '{datetime.datetime(2022,10,17)}'
                 LIMIT 1
             """)
             result = cursor.fetchall()
-    
-        
+
+
         s3_hook = S3Hook('s3_conn')
-        s3_hook.load_file(filename=f'{datetime.now()}', key=f'{datetime.now()}', bucket_name='airflow-testdata')
+        s3_hook.load_file(filename=f'{datetime.datetime.now()}', key=f'{datetime.datetime.now()}', bucket_name=BUCKET_NAME)
 
     fetch_and_export_relay_data = PythonOperator(
     task_id="fetch_and_export_relay_data",
     python_callable=_fetch_and_export_relay_data
     )
-    fetch_and_export_relay_data 
+    fetch_and_export_relay_data
