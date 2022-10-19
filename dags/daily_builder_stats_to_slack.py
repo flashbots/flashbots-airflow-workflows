5e5faa7184eed751d379d4ca6192a9ad1460fc7d
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2022-10-19 13:12:50 +0200

    Schedule daily at 8 utc, add builder description

--- dags/daily_builder_stats_to_slack.py
+++ dags/daily_builder_stats_to_slack.py
@@ -10,7 +10,7 @@ SLACK_CONN_ID = Variable.get('slack_conn_id')
 
 with DAG(
     dag_id = "builders_blocks_stats",
-    start_date = datetime.datetime(2022,10,19),
+    start_date = datetime.datetime(2022, 10, 20, 8),
     schedule_interval = "@daily",
     catchup = False,
 ) as dag:
@@ -18,7 +18,9 @@ with DAG(
     SqlToSlackOperator(
         task_id="daily_builders_stats_to_slack",
         sql_conn_id=POSTGRES_CONN_ID,
-        sql=f"SELECT builder_pubkey, count(builder_pubkey) as blocks FROM mainnet_payload_delivered WHERE inserted_at BETWEEN NOW() - INTERVAL '24 HOURS' AND NOW() GROUP BY builder_pubkey ORDER BY blocks DESC LIMIT 10",
+        sql=f"""SELECT a.builder_pubkey, a.blocks, b.description FROM (
+	SELECT builder_pubkey, count(builder_pubkey) as blocks FROM mainnet_payload_delivered WHERE inserted_at BETWEEN NOW() - INTERVAL '1 DAYS' AND NOW() GROUP BY builder_pubkey
+) a LEFT JOIN  mainnet_blockbuilder b ON a.builder_pubkey = b.builder_pubkey ORDER BY blocks DESC LIMIT 10""",
         slack_conn_id=SLACK_CONN_ID,
         slack_message="Builder stats {{ ds }}\n```{{ results_df | tabulate(tablefmt='pretty', headers='keys')}}```",
     )
