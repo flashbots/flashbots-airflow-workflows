8a01f48a6911cd01102de3c5da15d3d7ad197e7b
Gui Heise <gheise@ufl.edu>
2022-10-21 16:36:33 -0400

    Adjust catchup, query, secrets

--- dags/relay_export_dag.py
+++ dags/relay_export_dag.py
@@ -1,47 +1,39 @@
-import datetime
-import boto3
-import json
-import io
+from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.python import PythonOperator
 DAG_ID = "relay_export_dag"
 POSTGRES_CONN_ID = "boost_relay_read_replica"
-AWS_REGION = "us-east-2"
-SECRET = "airflow-airflow-main"
-
-session = boto3.session.Session()
-client = session.client(service_name='secretsmanager',region_name=AWS_REGION)
-secret = json.loads(client.get_secret_value(SecretId=SECRET)["SecretString"])
-
-BUCKET_NAME = secret["bucket_name"]
 
 with DAG(
     dag_id = DAG_ID,
-    start_date = datetime.datetime(2022,10,17),
-    schedule_interval = "@once",
+    start_date = datetime(2022,10,17),
+    schedule_interval = "@daily",
     catchup = True,
 ) as dag:
-
     def _fetch_and_export_relay_data():
-
+        import io
+        import csv
+        BUCKET_NAME = Variable.get('relay-data-export-bucket-name')
         psql_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         connection = psql_hook.get_conn()
         with connection.cursor() as cursor:
             cursor.execute(f"""
                 SELECT *
                 FROM mainnet_builder_block_submission
-                WHERE inserted_at > '{datetime.datetime(2022,10,17)}'
-                LIMIT 1
+                WHERE inserted_at > '{datetime.now()}'
+                AND inserted_at < '{datetime.now() - timedelta(hours=1)}'
             """)
-            result = cursor.fetchone()
+            result = cursor.fetchall()
 
         s3_hook = S3Hook('s3_conn')
-        s3_hook.load_file_obj(file_obj=io.StringIO(f"{result}".encode('UTF-8')), key=f'{datetime.datetime.now()}', bucket_name=BUCKET_NAME)
+        s3_hook.load_file_obj(file_obj=io.BytesIO(f"{result}".encode('UTF-8')), key=f'{datetime.now()}', bucket_name=BUCKET_NAME)
 
     fetch_and_export_relay_data = PythonOperator(
-    task_id="fetch_and_export_relay_data",
-    python_callable=_fetch_and_export_relay_data
+        task_id="fetch_and_export_relay_data",
+        python_callable=_fetch_and_export_relay_data
     )
+
     fetch_and_export_relay_data
