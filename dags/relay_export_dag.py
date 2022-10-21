4caf0611942764c4a412b0b390effc4336046d54
Gui Heise <gheise@ufl.edu>
2022-10-21 17:50:11 -0400

    Implement date-based catchup query

--- dags/relay_export_dag.py
+++ dags/relay_export_dag.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.models import Variable
+from airflow.models import dag
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.python import PythonOperator
 DAG_ID = "relay_export_dag"
-
 
 with DAG(
     dag_id = DAG_ID,
@@ -20,14 +20,15 @@ with DAG(
         BUCKET_NAME = Variable.get('relay-data-export-bucket-name')
         POSTGRES_CONN_ID = Variable.get('mainnet-relay-connection-id')
 
+        logical_date = "{{ ds }}"
         psql_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         connection = psql_hook.get_conn()
         with connection.cursor() as cursor:
             cursor.execute(f"""
                 SELECT *
                 FROM mainnet_builder_block_submission
-                WHERE inserted_at > '{datetime.now()}'
-                AND inserted_at < '{datetime.now() - timedelta(hours=1)}'
+                WHERE inserted_at > '{dag.start_date}'
+                AND inserted_at < '{dag.start_date - timedelta(hours=1)}'
             """)
             result = cursor.fetchall()
 
