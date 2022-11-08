472d114103efa29c243dee20bda342d2025d3d6e
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2022-11-08 12:26:59 +0100

    Push daily org stats to slack

--- dags/daily_builder_stats_to_slack.py
+++ dags/daily_builder_stats_to_slack.py
@@ -10,17 +10,34 @@ SLACK_CONN_ID = Variable.get('slack_conn_id')
 
 with DAG(
     dag_id = "builders_blocks_stats",
-    start_date = datetime.datetime(2022, 10, 20, 8),
-    schedule_interval = "@daily",
+    start_date = datetime.datetime(2022, 10, 20),
+    schedule_interval = '0 8 * * *', # daily 8 am utc
     catchup = False,
 ) as dag:
 
     SqlToSlackOperator(
         task_id="daily_builders_stats_to_slack",
         sql_conn_id=POSTGRES_CONN_ID,
-        sql=f"""SELECT a.builder_pubkey, a.blocks, b.description FROM (
-	SELECT builder_pubkey, count(builder_pubkey) as blocks FROM mainnet_payload_delivered WHERE inserted_at BETWEEN NOW() - INTERVAL '1 DAYS' AND NOW() GROUP BY builder_pubkey
-) a LEFT JOIN  mainnet_blockbuilder b ON a.builder_pubkey = b.builder_pubkey ORDER BY blocks DESC LIMIT 10""",
+        sql=f"""
+SELECT a.builder_pubkey, a.blocks, b.description FROM (
+    SELECT builder_pubkey, count(builder_pubkey) as blocks FROM mainnet_payload_delivered
+        WHERE inserted_at BETWEEN NOW() - INTERVAL '1 DAYS' AND NOW()
+        GROUP BY builder_pubkey) a
+    LEFT JOIN  mainnet_blockbuilder b ON a.builder_pubkey = b.builder_pubkey
+ORDER BY blocks DESC LIMIT 10""",
         slack_conn_id=SLACK_CONN_ID,
-        slack_message="Builder stats {{ ds }}\n```{{ results_df | tabulate(tablefmt='pretty', headers='keys')}}```",
+        slack_message="Builder stats for the last 24 hours\n```{{ results_df | tabulate(tablefmt='pretty', headers='keys')}}```",
+    )
+
+    SqlToSlackOperator(
+        task_id="daily_orgs_stats_to_slack",
+        sql_conn_id=POSTGRES_CONN_ID,
+        sql=f"""with desc_map (k, v) as (values ('fb-merger1', 'flashbots'), ('fb-merger2', 'flashbots'), ('fb-merger3', 'flashbots'), ('bloXroute max-profit', 'bloXroute'))
+SELECT coalesce(dm.v, nullif(b.description, ''), b.builder_pubkey) as org, sum(a.blocks) as blocks FROM ( SELECT builder_pubkey, count(builder_pubkey) as blocks
+FROM mainnet_payload_delivered WHERE inserted_at BETWEEN NOW() - INTERVAL '1 DAYS' AND NOW() GROUP BY builder_pubkey) a
+    LEFT JOIN  mainnet_blockbuilder b ON a.builder_pubkey = b.builder_pubkey
+    left join desc_map dm on dm.k = b.description
+group by coalesce(dm.v, nullif(b.description, ''), b.builder_pubkey) ORDER BY blocks DESC LIMIT 10""",
+        slack_conn_id=SLACK_CONN_ID,
+        slack_message="Orgs stats for the last 24 hours\n```{{ results_df | tabulate(tablefmt='pretty', headers='keys')}}```",
     )
